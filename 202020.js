395c5e69871379380b80b3a98ca9a96a93b29b4b
stan <stan.knezevic@gmail.com>
2021-04-30 02:28:43 -0700

    removing comments and changing interval to 20 minutes

--- 202020.js
+++ 202020.js
@@ -1,11 +1,10 @@
 
 const audioSource = document.getElementById("audioSource");
-const TWENTY_MINUTE_INTERVAL = 40000;//1200000;
+const TWENTY_MINUTE_INTERVAL = 1200000;
 const TWENTY_SECOND_INTERVAL = 20000;
 
 const playSound = (() => {
     clearTimeout(twentyMinuteInterval);
-    console.log("we in play sound");
     audioSource.play();
     start20MinuteTimer();
     clearTimeout(twentySecondInterval);
@@ -16,14 +15,11 @@ let twentySecondInterval = null;
 
 function start20MinuteTimer() {
     twentyMinuteInterval = setTimeout(() => {
-        // audioSource.setAttribute("muted", false);
-        console.log("we in here");
         twentySecondInterval = setTimeout(playSound, TWENTY_SECOND_INTERVAL);
     }, TWENTY_MINUTE_INTERVAL);
 }
 
 function stop20MinuteTimer() {
-    // audioSource.setAttribute("muted", true);
     clearTimeout(twentyMinuteInterval);
     clearTimeout(twentySecondInterval);
 }
